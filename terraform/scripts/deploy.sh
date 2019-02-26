6d03d30c280a4da1531b87c0f70a5f9e030daa9d
Amit Dali <12517713+amit-dali@users.noreply.github.com>
2019-02-26 21:58:08 +0100

    Update deploy.sh

--- terraform/scripts/deploy.sh
+++ terraform/scripts/deploy.sh
@@ -1,1 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+export KEY_PAIR_NAME=$1
+mkdir -p ~/amdali/minikube/terraform
+cd ~/amdali/minikube/terraform
+curl -O https://raw.githubusercontent.com/amit-dali/minikube/master/terraform/asg/asg.tf
+curl -O https://raw.githubusercontent.com/amit-dali/minikube/master/terraform/asg/outputs.tf
+curl -O https://raw.githubusercontent.com/amit-dali/minikube/master/terraform/asg/startup.sh
+curl -O https://raw.githubusercontent.com/amit-dali/minikube/master/terraform/asg/variables.tf
+
+terraform -version
+terraform init
+terraform plan -refresh=true -var "key_name=${KEY_PAIR_NAME}"
+terraform apply -var "key_name=${KEY_PAIR_NAME}" -auto-appro
