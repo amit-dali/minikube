47a08d0ffb5535afa0b74dad21f0b4a920033f86
Amit Dali <12517713+amit-dali@users.noreply.github.com>
2019-02-26 20:09:32 +0100

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,13 +4,11 @@ chmod 755 -R amdali
 cd amdali/assignment1
 curl -O https://raw.githubusercontent.com/amit-dali/minikube/master/ansible/assignment1.yml && chmod 0755 assignment1.yml
 apt-get update
-apt-get install software-properties-common
+apt-get --assume-yes install software-properties-common docker-compose python-pip
 apt-add-repository --yes --update ppa:ansible/ansible
-apt-get install ansible
+apt-get --assume-yes install ansible 
 echo "127.0.0.1" > ~/ansible_hosts
 export ANSIBLE_INVENTORY=~/ansible_hosts
-apt-get install docker-compose
-apt install python-pip
-pip install openshift
-pip install --upgrade requests
+apt-get --assume-yes install docker-compose
+pip install openshift requests
 ansible-playbook assignment1.yml -i 'local,' --connection=local
