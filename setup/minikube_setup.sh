2d81e6d7d5f050d2947f3b8929df76ebf13254a1
Amit Dali <12517713+amit-dali@users.noreply.github.com>
2019-02-26 13:32:17 +0100

    Update minikube_setup.sh

--- setup/minikube_setup.sh
+++ setup/minikube_setup.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-mkdir -p ~/amdali/assignment1
-chmod 755 -R amdali
-cd amdali/assignment1
-curl -O https://raw.githubusercontent.com/amit-dali/minikube/master/ansible/assignment1.yml && chmod 0755 assignment1.yml
 apt-get update -y &&  apt-get install -y docker.io
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.8.0/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
