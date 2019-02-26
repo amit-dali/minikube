17db80a8d3afe650a469580343ccd4bbb33ab5a8
Amit Dali <12517713+amit-dali@users.noreply.github.com>
2019-02-26 20:56:57 +0100

    Create startup.sh

---
+++ terraform/asg/startup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -O https://github.com/amit-dali/minikube/blob/master/setup/minikube_setup.sh && chmod 0755 minikube_setup.sh
+curl -O https://github.com/amit-dali/minikube/blob/master/setup/setup.sh && chmod 0755 setup.sh
