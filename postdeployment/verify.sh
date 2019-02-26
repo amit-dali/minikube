423a387d938a73d1def30d19e9977caa76e11ce4
Amit Dali <12517713+amit-dali@users.noreply.github.com>
2019-02-26 17:16:47 +0100

    Update verify.sh

--- postdeployment/verify.sh
+++ postdeployment/verify.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "nginx-service url=" | minikube service nginx-service -n demo1 --url
-echo "tomcat-service url=" | minikube service tomcat-service -n demo1 --url
+echo "nginx-service url=" & minikube service nginx-service -n demo1 --url
+echo "tomcat-service url=" & minikube service tomcat-service -n demo1 --url
