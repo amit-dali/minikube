a5cd48475eb336b386a5fc2fac2b65f69913e9e8
Amit Dali <12517713+amit-dali@users.noreply.github.com>
2019-02-27 17:17:34 +0100

    Update startup.sh

--- terraform/asg/startup.sh
+++ terraform/asg/startup.sh
@@ -2,6 +2,8 @@
 curl -O https://raw.githubusercontent.com/amit-dali/minikube/master/setup/minikube_setup.sh && chmod 0755 minikube_setup.sh
 curl -O https://raw.githubusercontent.com/amit-dali/minikube/master/setup/setup.sh && chmod 0755 setup.sh
 bash minikube_setup.sh
-bash setup.sh
 /usr/local/bin/minikube stop
 /usr/local/bin/minikube start --vm-driver=none
+bash setup.sh
+
+
